b9fac9f0427b10984fee5e78e38e1530b9c1dca1
mznaim <mznaimcse@gmail.com>
2019-09-24 02:36:53 +0600

    using cpp string functinos

---
+++ 272-Tex-Quote.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	string str;
+	int flag = 1;
+	while(getline(cin, str)){
+		for(int i = 0; i < str.size(); i++){
+			if(str[i] == '\"')
+				if(flag % 2 == 1){
+					str.replace(i,1,"``");
+					flag++;
+				}
+				else{
+					str.replace(i,1,"''");
+					flag++;
+				}
+		}
+		cout << str << endl;
+
+	}
+
+}
